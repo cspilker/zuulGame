ac0b58c1b8b974e0813d98580faf42b62e90c555
cspilker <cdrc.splkr@gmail.com>
2020-01-28 16:51:36 +0100

    Removed duplicated code in Room.java

--- Zuul-bad/src/de/nordakademie/zuul/Game.java
+++ Zuul-bad/src/de/nordakademie/zuul/Game.java
@@ -82,22 +82,10 @@ public class Game
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
         System.out.println("Type 'help' if you need help.");
         System.out.println();
-        System.out.println("You are " + currentRoom.getDescription());
-        System.out.print("Exits: ");
-        if(currentRoom.northExit != null) {
-            System.out.print("north ");
-        }
-        if(currentRoom.eastExit != null) {
-            System.out.print("east ");
-        }
-        if(currentRoom.southExit != null) {
-            System.out.print("south ");
-        }
-        if(currentRoom.westExit != null) {
-            System.out.print("west ");
-        }
-        System.out.println();
+        currentRoom.printLocationInfo();
     }
+
+	
 
     /**
      * Given a command, process (that is: execute) the command.
@@ -160,16 +148,16 @@ public class Game
         // Try to leave current room.
         Room nextRoom = null;
         if(direction.equals("north")) {
-            nextRoom = currentRoom.northExit;
+            nextRoom = currentRoom.getNorthExit();
         }
         if(direction.equals("east")) {
-            nextRoom = currentRoom.eastExit;
+            nextRoom = currentRoom.getEastExit();
         }
         if(direction.equals("south")) {
-            nextRoom = currentRoom.southExit;
+            nextRoom = currentRoom.getSouthExit();
         }
         if(direction.equals("west")) {
-            nextRoom = currentRoom.westExit;
+            nextRoom = currentRoom.getWestExit();
         }
 
         if (nextRoom == null) {
@@ -177,21 +165,8 @@ public class Game
         }
         else {
             currentRoom = nextRoom;
-            System.out.println("You are " + currentRoom.getDescription());
-            System.out.print("Exits: ");
-            if(currentRoom.northExit != null) {
-                System.out.print("north ");
-            }
-            if(currentRoom.eastExit != null) {
-                System.out.print("east ");
-            }
-            if(currentRoom.southExit != null) {
-                System.out.print("south ");
-            }
-            if(currentRoom.westExit != null) {
-                System.out.print("west ");
-            }
-            System.out.println();
+            
+            currentRoom.printLocationInfo();
         }
     }
 
--- Zuul-bad/src/de/nordakademie/zuul/Room.java
+++ Zuul-bad/src/de/nordakademie/zuul/Room.java
@@ -1,67 +1,89 @@
 package de.nordakademie.zuul;
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  * Class Room - a room in an adventure game.
  *
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
+ * This class is part of the "World of Zuul" application. "World of Zuul" is a
+ * very simple, text based adventure game.
  *
- * A "Room" represents one location in the scenery of the game.  It is 
- * connected to other rooms via exits.  The exits are labelled north, 
- * east, south, west.  For each direction, the room stores a reference
- * to the neighboring room, or null if there is no exit in that direction.
+ * A "Room" represents one location in the scenery of the game. It is connected
+ * to other rooms via exits. The exits are labelled north, east, south, west.
+ * For each direction, the room stores a reference to the neighboring room, or
+ * null if there is no exit in that direction.
  * 
- * @author  Michael Kölling and David J. Barnes
+ * @author Michael Kölling and David J. Barnes
  * @version 2016.02.29
  */
-public class Room 
-{
-    public String description;
-    public Room northExit;
-    public Room southExit;
-    public Room eastExit;
-    public Room westExit;
+public class Room {
+	private String description;
+	private Map<String, Room>exits = new HashMap<>();
+	
+	
 
-    /**
-     * Create a room described "description". Initially, it has
-     * no exits. "description" is something like "a kitchen" or
-     * "an open court yard".
-     * @param description The room's description.
-     */
-    public Room(String description) 
-    {
-        this.description = description;
-    }
+	public Room getNorthExit() {
+		return exits.get("north");
+	}
 
-    /**
-     * Define the exits of this room.  Every direction either leads
-     * to another room or is null (no exit there).
-     * @param north The north exit.
-     * @param east The east east.
-     * @param south The south exit.
-     * @param west The west exit.
-     */
-    public void setExits(Room north, Room east, Room south, Room west) 
-    {
-        if(north != null) {
-            northExit = north;
-        }
-        if(east != null) {
-            eastExit = east;
-        }
-        if(south != null) {
-            southExit = south;
-        }
-        if(west != null) {
-            westExit = west;
-        }
-    }
+	public Room getSouthExit() {
+		return exits.get("south");
+	}
 
-    /**
-     * @return The description of the room.
-     */
-    public String getDescription()
-    {
-        return description;
-    }
+	public Room getEastExit() {
+		return exits.get("east");
+	}
+
+	public Room getWestExit() {
+		return exits.get("west");
+	}
+
+	/**
+	 * Create a room described "description". Initially, it has no exits.
+	 * "description" is something like "a kitchen" or "an open court yard".
+	 * 
+	 * @param description The room's description.
+	 */
+	public Room(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * Define the exits of this room. Every direction either leads to another room
+	 * or is null (no exit there).
+	 * 
+	 * @param north The north exit.
+	 * @param east  The east east.
+	 * @param south The south exit.
+	 * @param west  The west exit.
+	 */
+	public void setExits(Room north, Room east, Room south, Room west) {
+		if (north != null) {
+			exits.put("north", north);
+		}
+		if (east != null) {
+			exits.put("east", east);
+		}
+		if (south != null) {
+			exits.put("south", south);
+		}
+		if (west != null) {
+			exits.put("west", west);
+		}
+	}
+
+	public void printLocationInfo() {
+		System.out.println("You are " + this.description);
+		System.out.print("Exits: ");
+		
+		for(String exit: exits.keySet()){
+		System.out.println(exit+" ");	
+		}
+		System.out.println();
+	}
+
+	/**
+	 * @return The description of the room.
+	 */
 
 }
